1b89003664ca9228fe16e0001358f5a9f480b6c3
cuongvu3009 <cuongvu3009@gmail.com>
2022-09-06 16:34:21 +0300

    make it better

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 // only when ready to deploy
 app.use(express.static(path.resolve(__dirname, './client/build')));
 
+//	Security Packages
 app.use(express.json());
 app.use(helmet());
 app.use(xss());
